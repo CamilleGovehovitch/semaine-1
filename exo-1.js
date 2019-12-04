6c8dadbeb3ecb91d767f3d888d5a9f28d2004ee5
Camille Govehovitch <camillegovehovitch@air-de-camille.home>
2019-12-04 15:02:44 +0100

    exo-1 html + exo 2 template

--- exo-1.js
+++ exo-1.js
@@ -1,10 +1,19 @@
 console.log('hello world');
-let a = 3;
-let b = 2;
-let c;
-console.log('a = ' + a,'b = ' + b);
-c = a;
-a = b;
-console.log('c = ' + c, 'a = ' + a);
-b = c;
-console.log('a = ' + a, 'b = ' +b);
+ function changeVariable(a,b){
+     let c;
+     c = a;
+     a = b;
+     b = c;
+     return {
+         a: a,
+         b: b
+     }
+ }
+let a = 1;
+let b = 8;
+let res = changeVariable(a,b);
+
+document.getElementById('a').innerHTML = 'A = ' + a;
+document.getElementById('b').innerHTML = 'B = ' + b;
+document.getElementById('aPrime').innerHTML = 'A = ' + res.a;
+document.getElementById('bPrime').innerHTML = 'B = ' + res.b;
