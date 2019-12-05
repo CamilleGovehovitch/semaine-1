c647cc8f5f52f11b7b71619cd4db63f66964b1fa
Camille Govehovitch <camillegovehovitch@MacBook-Air-de-Camille.local>
2019-12-05 14:20:25 +0100

    exo-7 unchecked

--- exo-7.js
+++ exo-7.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+let inputOne = document.getElementById('inputOne');
+let inputTwo = document.getElementById('inputTwo');
+let nbsOne = inputOne;
+let nbsTwo = inputTwo;
+
+
+function prod(){
+    if(parseInt(nbsOne.value) < 0 && parseInt(nbsTwo.value) > 0){
+        document.getElementById('result').innerHTML = 'Le produit est négatif';
+    }else if(parseInt(nbsOne.value) > 0 && parseInt(nbsTwo.value) < 0){
+        document.getElementById('result').innerHTML = 'Le produit est négatif';
+    }else{
+        document.getElementById('result').innerHTML = 'Le produit est positif';
+
+    }
+    return;
+}
