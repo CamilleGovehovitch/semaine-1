73b9f27917c40bb85586d96cbeae481f81ff012a
Camille Govehovitch <camillegovehovitch@MacBook-Air-de-Camille.local>
2019-12-06 15:34:47 +0100

    exo finished but un checked

--- exo-12.js
+++ exo-12.js
@@ -3,6 +3,12 @@ let minute = document.getElementById('input2');
 let result = document.getElementById('result');
 function futur(){
     let minutePlus = parseInt(minute.value, 10) + 1;
-    result.innerHTML = "Dans une minute il sera " + hour.value + " heures et " + minutePlus + " minutes";
-    return 'hello';
+    if(minutePlus > 59){
+        let hourPlus = parseInt(hour.value,10) + 1;
+        minutePlus = 0;
+        result.innerHTML = "Dans une minute il sera " + hourPlus + " heures et " + minutePlus + " minutes";
+    }else{
+        result.innerHTML = "Dans une minute il sera " + hour.value + " heures et " + minutePlus + " minutes";
+    }
+
 }
