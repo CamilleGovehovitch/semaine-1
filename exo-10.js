1cbad318c26cf99cd69a09704dd8ef6cd282466b
Camille Govehovitch <camillegovehovitch@MacBook-Air-de-Camille.local>
2019-12-06 11:24:00 +0100

    exo-10 cleaned

--- exo-10.js
+++ exo-10.js
@@ -2,7 +2,6 @@ let num1 = document.getElementById('input1');
 let num2 = document.getElementById('input2');
 let result = document.getElementById('result');
 function test(){
-    console.log(num1.value, num2.value);
     if((num1.value < 0 && num2.value > 0)||(num1.value > 0 && num2.value < 0)){
         result.innerHTML = "-";
     }else if((num1.value > 0 && num2.value > 0)||(num1.value < 0 && num2.value < 0)){
@@ -10,5 +9,5 @@ function test(){
     }else{
         result.innerHTML = 'NULL';
     }
-
+    return
 }
